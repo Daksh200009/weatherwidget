18de594a4f173aba75ff3d1e1e95c5b37e13b6d7
Daksh2009 <chandd@pedarecc.sa.edu.au>
2024-05-24 11:01:27 +0930

    Template of the code including card

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-from flask import Flask, render_template, request
-from datetime import datetime
-from matplotlib import pyplot as plt
 import requests
+from datetime import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 
-
-    return render_template('home.html')
-
-
-def get_weather(city):
-    api_key = "95811ce11bed9885114029457eccb350"
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&unit=metric"
+    api_key = '95811ce11bed9885114029457eccb350'
+    city = 'Adelaide'
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + api_key
     print(url)
     response = requests.get(url)
     data = response.json()
-    return render_template("home.html")
+    city_name = data['city']['name']
+    city_population = data['city']['population']
+    print('city_name')
+    print('city_population')
 
-    # "description": data["weather"][0]["description"],
+    #'description': data['weather'][0]['description'],
 
+    forecast_list = data['list']
+    index = 0
+    while index < len(forecast_list):
 
+        print(forecast_list[index]['dt_txt'])
+        print(forecast_list[index]['main']['weather'])
+        print(forecast_list[index]['weather'][0]['temp'])
+        print(forecast_list[index]['weather'][0]['main'])
+        index += 8
 
+    #   print('server_time' + forecast['dt_txt'])
+    #   print('local' + str(datetime.fromtimestamp(forecast['dt'])))
+    #   print(forecast['main']['temp_min'])
+    #   print(forecast['main']['temp_max'])
+    #
 
-# forecast_list = response['list'[0]]
-# for forecast in forecast_list:
-#     timestap
-# print(get_weather("adelaide"))
-
+    forecast_list = ""
+    return render_template('home.html', forecast_list=forecast_list)
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
